27f93dc72539d642f608371bec64714af17ef5f6
amund-fremming <amund.fremming@gmail.com>
2023-09-09 20:00:46 +0200

    Added more apis

--- todo-backend/src/main/java/todobackend/todo/controller/TodoController.java
+++ todo-backend/src/main/java/todobackend/todo/controller/TodoController.java
@@ -1,25 +1,23 @@
 package todobackend.todo.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import todobackend.todo.model.Todo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(
-        origins = "http://localhost:5173",
-        methods = {RequestMethod.GET, RequestMethod.POST},
+        origins = {"http://localhost:5173","http://localhost:8080"},
+        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE},
         allowedHeaders = {"Content-Type", "Authorization"}
 )
 public class TodoController {
 
-    private List<Todo> todos = List.of(
-            new Todo(1, "First", "Clean you room"),
-            new Todo(2, "Second", "Shop clothes"),
-            new Todo(3, "Third", "Make dinner")
-    );
+    private List<Todo> todos = new ArrayList<Todo>();
 
     @ResponseBody
     @GetMapping("/fetch-todos")
@@ -27,14 +25,31 @@ public class TodoController {
         return todos;
     }
 
+    @ResponseBody
     @PostMapping("/push-todo")
-    public String pushTodoData(/* Data inn */) {
-        return "pushed todo to db";
+    public ResponseEntity<String> pushTodoData(@RequestBody Todo todo) {
+        todos.add(todo);
+        return ResponseEntity.ok("TODO added successfully");
     }
 
-    @PutMapping("update-todo")
-    public String updateTodoData(/* Data inn */) {
-        return "updated todo in db";
+    @ResponseBody
+    @PutMapping("/update-todo/{id}")
+    public ResponseEntity<String> updateTodoData(@PathVariable int id, @RequestBody Todo updatedTodo) {
+        System.out.println(id);
+        todos.stream().forEach(todo -> {
+            if(todo != null && todo.getTodoid() == id)
+                todos.remove(todo);
+        });
+        todos.add(updatedTodo);
+        return ResponseEntity.ok("TODO Updated successfully");
+    }
+
+    @ResponseBody
+    @DeleteMapping("/delete-todo/{id}")
+    public ResponseEntity<String> deleteTodoData(@PathVariable int id) {
+        System.out.println(id);
+        // Add code
+        return ResponseEntity.ok("TODO Deleted successfully");
     }
 
 }
